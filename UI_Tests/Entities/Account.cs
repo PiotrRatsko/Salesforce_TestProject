0fb287ab108c309baefda87eb0938b80bb756af7
Piotr Ratsko <piotr.ratsko@gmail.com>
2021-09-16 13:46:41 +0300

    Add assertion for accounts

--- UI_Tests/Entities/Account.cs
+++ UI_Tests/Entities/Account.cs
@@ -4,10 +4,14 @@ namespace UI_Tests.Entities
 {
     class Account
     {
+        [Display(Name = "Account Name")]
         [Required(AllowEmptyStrings = false)]
         public string AccountName { get; set; }
+
         [RegularExpression("Analyst|Customer|Investor")]
         public string Type { get; set; }
+
+        [Display(Name = "Description")]
         public string Description { get; set; }
     }
 }
--- UI_Tests/PageObject/AccountsPage.cs
+++ UI_Tests/PageObject/AccountsPage.cs
@@ -1,6 +1,9 @@
 ﻿using OpenQA.Selenium;
 using Selenium_TestFrameWork;
 using Selenium_TestFrameWork.WebDriverExtention;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using UI_Tests.Entities;
 
 namespace UI_Tests.PageObject
 {
@@ -8,6 +11,9 @@ namespace UI_Tests.PageObject
     {
         #region IWebElements
         private readonly By NewAccountBtn = By.CssSelector("a[class='forceActionLink'][title='New']"); //new account button
+        private readonly By AccountDetailsBtn = By.XPath("//li[@title='Details']");
+        private readonly string AccountDetailsField = "//div[@class='slds-form']//span[text()='{0}']/../following-sibling::div//lightning-formatted-text";
+        private readonly string AccountNameLink = "(//a[@title='{0}'])[1]"; //find account by account name
         #endregion IWebElements
 
         public AccountsPage(IWebDriver _driver) : base(_driver)
@@ -21,8 +27,34 @@ namespace UI_Tests.PageObject
         #region Actions
         public NewAccountPage ClickNewAccountBtn()
         {
+            driver.WaitForTitle(PageTitle);
             driver.ClickButton(NewAccountBtn);
             return new NewAccountPage(driver);
+        }
+
+        public AccountsPage AddNewAccount(Account account)
+        {
+            ClickNewAccountBtn().FillAndSaveNewAccount(account);
+            return ClickAccountsBtn();
+        }
+
+        public Account GetAccount(string accountName)
+        {
+            LogHelper.log.Info("Getting account by accountName");
+            driver.WaitForTitle(PageTitle);
+            var formatedXPath = string.Format(AccountNameLink, accountName);
+            driver.GetElement(By.XPath(formatedXPath)).Click();
+            driver.WaitForTitle($"{accountName} | Salesforce");
+            driver.ClickButton(AccountDetailsBtn);
+            Account account = new();
+            foreach (var piInstance in account.GetType().GetProperties())
+            {
+                string propName = piInstance.GetCustomAttribute<DisplayAttribute>()?.Name;
+                propName ??= piInstance.Name;
+                formatedXPath = string.Format(AccountDetailsField, propName);
+                piInstance.SetValue(account, driver.GetElement(By.XPath(formatedXPath)).Text);
+            }
+            return account;
         }
         #endregion Actions 
     }
