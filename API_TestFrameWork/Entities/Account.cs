47029cfd1a944b2bf3e0c2235e402fe1396bedb1
Piotr Ratsko <piotr.ratsko@gmail.com>
2021-09-17 12:08:56 +0300

    commit

---
+++ API_TestFrameWork/Entities/Account.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_TestFrameWork.Entities
+{
+    class Account
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string AccountName { get; set; }
+        [RegularExpression("Analyst|Customer|Investor")]
+        public string Type { get; set; }
+        public string Description { get; set; }
+    }
+}
